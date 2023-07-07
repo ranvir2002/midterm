37f3a80d3a25bb94979368223c20b519d5814fd2
ranvir2002 <bsbp414@gmail.com>
2023-07-07 12:45:54 -0400

    project

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+url:"mongodb+srv://Ranvir:Ranvir2002@cluster0.lsbgwtv.mongodb.net/DressStore?retryWrites=true&w=majority"
+
+};
